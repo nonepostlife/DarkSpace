05d411be9fe0f17a16f79ffcd9a430bebd8fb6cf
nonepostlife <ilyha575@inbox.ru>
2021-07-28 16:00:47 +1000

    Initial commit

---
+++ desktop/src/ru/postlife/gdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.postlife.gdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.postlife.gdx.DarkSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DarkSpace(), config);
+	}
+}

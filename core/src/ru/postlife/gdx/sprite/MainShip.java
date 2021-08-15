b403623b7dfca6ae923184169ae5003631a2a817
nonepostlife <ilyha575@inbox.ru>
2021-08-15 15:17:48 +1000

    added hw6

--- core/src/ru/postlife/gdx/sprite/MainShip.java
+++ core/src/ru/postlife/gdx/sprite/MainShip.java
@@ -14,7 +14,7 @@ public class MainShip extends Ship {
     private static final float HEIGHT = 0.15f;
     private static final float BOTTOM_MARGIN = 0.05f;
     private static final int INVALID_POINTER = -1;
-    private static final float RELOAD_INTERVAL = 0.2f;
+    private static final float RELOAD_INTERVAL = 0.5f;
 
     private boolean pressedLeft;
     private boolean pressedRight;
